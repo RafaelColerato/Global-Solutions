4b3122615c6b7991e2c588de262fcef85ced28d6
Rafael Colerato <rafael.colerato@outlook.com>
2023-06-05 08:45:19 -0300

    Add files via upload

---
+++ gs php/footer.php
@@ -0,0 +1,12 @@
+<footer>
+    <p>
+        Rafael Colerato
+    </p>
+    <p>
+        <a href="https://github.com/RafaelColerato"> GIT HUB </a>
+    </p>
+    <p>
+        <a href="https://www.linkedin.com/in/rafael-colerato/"> LINKEDIN </a>
+    </p>
+
+</footer>
